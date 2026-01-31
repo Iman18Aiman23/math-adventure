2c7b025805501672956c80c398f7be57921b1ebc
Iman18Aiman23 <rosliyussof@gmail.com>
2026-01-31 12:36:56 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/math-adventure/', // REPLACE 'your-repo-name' with your actual GitHub project name
   plugins: [react()],
+  base: '/math-adventure/', // This must match your repo name exactly!
 })
